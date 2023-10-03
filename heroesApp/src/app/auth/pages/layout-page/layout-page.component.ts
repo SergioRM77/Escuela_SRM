e1b3020b1aa8351b1951e09fd522c95eb7e34db0
SergioRM77 <sergio93rm@gmail.com>
2023-10-03 12:48:57 +0200

    feat: added first part about HeroApp project

---
+++ heroesApp/src/app/auth/pages/layout-page/layout-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout-page.component.html',
+  styles: [
+  ]
+})
+export class LayoutPageComponent {
+
+}
